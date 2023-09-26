1fbb9d0444c2a9c31cbad522630118001a8c4453
afeiship <1290657123@qq.com>
2023-09-26 09:15:54 +0800

    feat: add sw support for lskit

--- docs/lskit.js
+++ docs/lskit.js
@@ -3,8 +3,17 @@
  * description: Pure loading-screen for spa.
  * homepage: https://github.com/afeiship/loading-screen
  * version: 1.0.4
- * date: 2022-09-22 22:58:02
+ * date: 2023-09-26 09:15:13
  * license: MIT
  */
 
-window.lskit=function(i){var i=Object.assign({id:"root",lang:"language","en-US":"Loading...","zh-CN":"应用加载中..."},i),n=(document.getElementById(i.id).innerHTML='<section class="loading-screen"><div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div><span id="loading"></span></section>',document.querySelector("#loading")),i=new URL(window.location.href).searchParams.get(i.lang);n.innerHTML="en-US"===i?"Loading...":"应用加载中..."};
+window.lskit=function(n){function e(n){var e=new URLSearchParams(window.location.search).get(i.lang)||navigator.language||"en-US";return o[e][n]}const i={id:"root",lang:"language","en-US":"Loading...","zh-CN":"应用加载中...",sw:0},o={"en-US":{loading:"Loading...","load-error":"⚠️ Load error, reload"},"zh-CN":{loading:"应用加载中...","load-error":"⚠️ 加载错误，重试"}};n=Object.assign(i,n);document.getElementById(n.id).innerHTML=`<section class="loading-screen">
+    <div class="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+    <span id="loading"></span>
+  </section>`;const r=document.querySelector("#loading"),a=document.querySelector(".loading-screen");r.innerHTML=e("loading"),n.sw&&(setTimeout(function(){r.innerHTML=e("load-error"),a.classList.add("load-error")},n.sw),a.addEventListener("click",function(){r.classList.contains("load-error")&&navigator.serviceWorker.getRegistration("/").then(function(n){n&&n.unregister(),window.location.reload()})}))};
--- src/lskit.js
+++ src/lskit.js
@@ -1,15 +1,61 @@
 window.lskit = function (options) {
   const defaults = {
-    id: "root",
-    lang: "language",
-    "en-US": "Loading...",
-    "zh-CN": "应用加载中...",
+    'id': 'root',
+    'lang': 'language',
+    'en-US': 'Loading...',
+    'zh-CN': '应用加载中...',
+    'sw': 0
   };
+
+  const locales = {
+    'en-US': {
+      'loading': 'Loading...',
+      'load-error': '⚠️ Load error, reload'
+    },
+    'zh-CN': {
+      'loading': '应用加载中...',
+      'load-error': '⚠️ 加载错误，重试'
+    }
+  };
+
+  const t = function (key) {
+    const params = new URLSearchParams(window.location.search);
+    const lang = params.get(defaults.lang) || navigator.language || 'en-US';
+    return locales[lang][key];
+  };
+
   const opts = Object.assign(defaults, options);
   const root = document.getElementById(opts.id);
-  root.innerHTML = `<section class="loading-screen"><div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div><span id="loading"></span></section>`;
 
-  const el = document.querySelector("#loading");
-  const lang = new URL(window.location.href).searchParams.get(opts.lang);
-  el.innerHTML = lang === "en-US" ? "Loading..." : "应用加载中...";
+  root.innerHTML = `<section class="loading-screen">
+    <div class="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+    <span id="loading"></span>
+  </section>`;
+
+  const el = document.querySelector('#loading');
+  const lsRoot = document.querySelector('.loading-screen');
+  el.innerHTML = t('loading');
+
+  if (opts.sw) {
+    // wait 30s disappear
+    setTimeout(function () {
+      el.innerHTML = t('load-error');
+      lsRoot.classList.add('load-error');
+    }, opts.sw);
+
+    lsRoot.addEventListener('click', function () {
+      const hasError = el.classList.contains('load-error');
+      if (!hasError) return;
+      navigator.serviceWorker.getRegistration('/').then(function (e) {
+        e && e.unregister();
+        window.location.reload();
+      });
+    });
+  }
 };
